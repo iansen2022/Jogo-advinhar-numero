4f39826a895350c7eded92930293e46cc6c2b283
Samuel Iansen <iansen2022@gmail.com>
2023-03-06 23:38:07 -0300

    jogo adivinhar numero

---
+++ Jogo2.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.random.*;
+
+public class Jogo2 {
+
+	public static void main(String[] args) {
+		System.out.println("");
+
+	}
+
+}
