de25a505a37cfe3fb5514db68152607e789e6171
Samuel Iansen <iansen2022@gmail.com>
2023-03-07 23:17:51 -0300

    jogo adivinhar número

---
+++ Jogo.java
@@ -0,0 +1,23 @@
+package jogo;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Random.*;
+
+public class Jogo {
+
+	public static void main(String[] args) {
+
+		PrintStream show = System.out;
+		Scanner scan = new Scanner(System.in);
+		Random gerador = new Random();
+		
+		int opcao;
+		int escolha_computador;
+		int escolha_usuario;
+		
+		show.println("Olá! Digite seu nome?");
+	}
+
+}
